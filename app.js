5b9ee7999f1c2fe95cfe3c3d0d3b9312b45649e0
Jason Moy <jasonmoy28@gmail.com>
2020-12-10 11:26:07 +0800

    type: <feat> action: <added world clock feature>

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
 const modeCircle = document.querySelector(".circle");
 const modeOval = document.querySelector(".oval");
 const timeHTML = document.querySelector(".time");
+const worldTime = document.querySelector(".world-time");
 
 modeCircle.addEventListener("click", () => {
   modeCircle.classList.toggle("circle-clicked");
   modeOval.classList.toggle("oval-clicked");
 });
 
-const time = new Date();
-let currentHours = time.getHours();
-let AMPM = "";
-if (currentHours < 12) {
-  AMPM = "AM";
-} else {
-  currentHours = currentHours - 12;
-  AMPM = "PM";
+class currentTime {
+  getUTCCurrentTime() {
+    const time = new Date();
+    let currentUTCHours = time.getUTCHours();
+    let currentUTCMinutes = time.getUTCMinutes();
+    return [currentUTCHours, currentUTCMinutes];
+  }
+  formatHour(hour) {
+    let APM = "";
+    if (hour > 12) {
+      hour = hour - 12;
+      APM = "PM";
+    } else {
+      APM = "AM";
+    }
+    return [hour, APM];
+  }
+  formatMinute(minute) {
+    if (minute < 10) {
+      minute = "0" + minute;
+    }
+    return minute;
+  }
+  convertToLocalTime(utc_offset) {
+    let localHour = this.getUTCCurrentTime()[0] + utc_offset;
+    if (localHour > 24) {
+      localHour -= 24;
+    } else if (localHour < 0) {
+      localHour += 24;
+    } else if (localHour == 0) {
+      localHour += 12;
+    }
+    const localMinute = this.formatMinute(this.getUTCCurrentTime()[1]);
+    let localHourArray = this.formatHour(localHour);
+    const localTime =
+      localHourArray[0] + ":" + localMinute + " " + localHourArray[1];
+    return localTime;
+  }
 }
 
-let currentMinutes = time.getMinutes();
-
-if (currentMinutes < 10) {
-  currentMinutes = "0" + currentMinutes;
-}
-
-currentTime = currentHours + ":" + currentMinutes + " " + AMPM;
-console.log(timeHTML);
-timeHTML.innerText = currentTime;
+const localTime = new currentTime();
+timeHTML.innerText = localTime.convertToLocalTime(8);
+worldTime.children[0].children[0].innerText = localTime.convertToLocalTime(8);
+worldTime.children[1].children[0].innerText = localTime.convertToLocalTime(-8);
+worldTime.children[2].children[0].innerText = localTime.convertToLocalTime(-5);
